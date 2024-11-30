1e533a32919eee359270a7e5f6048d06b5d29047
FriedAxons <friedaxons@proton.me>
2024-11-30 14:17:00 -0600

    Add yml workflow and configure vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/product-list-with-cart/",
+  resolve: {
+    alias: {
+      "@": "./src",
+    },
+  },
+  build: {
+    outDir: "dist",
+  },
+});
