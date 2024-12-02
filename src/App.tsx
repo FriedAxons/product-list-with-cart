1b9991ab587a0cc2ba27597705c0f8ac15c20369
FriedAxons <friedaxons@proton.me>
2024-12-02 15:00:07 -0600

    Create product interface and states

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
+import { useState } from "react";
+import productData from "./data/data.json";
 import Header from "./components/Header";
 // import ProductList from "./components/ProductList";
 // import Cart from "./components/Cart";
-// import productsData from "./data/data.json";
+// import Modal from "./components/Modal";
+
+interface Product {
+  image: {
+    thumbnail: string;
+    mobile: string;
+    tablet: string;
+    desktop: string;
+  };
+  name: string;
+  category: string;
+  price: number;
+}
 
 const App: React.FC = () => {
+  const [products] = useState<Product[]>(productData);
+  const [cart, setCart] = useState<{ product: Product; quantity: number }[]>(
+    []
+  );
+  const [showModal, setShowModal] = useState<boolean>(false);
+
   return (
-    <div className="App">
+    <div>
       <Header />
     </div>
   );
