b113f5716483929bbcbac4b4df30a050872a7159
FriedAxons <friedaxons@proton.me>
2024-12-06 23:39:01 -0600

    Conditional rendering in Cart component
    Refactored Cart component to conditionally render
    and display a image and text when the cart is
    empty. As opposed to what is in the else statement
    now and how that will handle rendering the cart
    when items are added.

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,6 +1,7 @@
 import { Product } from "../App";
 import removeIcon from "/product-list-with-cart/images/icon-remove-item.svg";
 import carbonNeutralIcon from "/product-list-with-cart/images/icon-carbon-neutral.svg";
+import emptyCartIcon from "/product-list-with-cart/images/illustration-empty-cart.svg";
 
 interface CartProps {
   cartItems: { product: Product; quantity: number }[];
@@ -16,33 +17,46 @@ const Cart = ({
   confirmOrder,
 }: CartProps) => (
   <div className="cart">
-    {cartItems.map(({ product, quantity }) => (
-      <div key={product.name} className="cart-item">
-        <div className="cart-item-details">
-          <p>{product.name}</p>
-          <p>
-            {quantity}x @ ${product.price.toFixed(2)} $
-            {(product.price * quantity).toFixed(2)}
-          </p>
-        </div>
-        <button
-          onClick={() => removeFromCart(product.name)}
-          className="remove-button"
-        >
-          <img src={removeIcon} alt="Remove Icon" width={24} height={24} />
-        </button>
+    {cartItems.length === 0 ? (
+      <div className="empty-cart">
+        <img
+          src={emptyCartIcon}
+          alt="Empty Cart Icon"
+          className="empty-cart-icon"
+        />
+        <p>Your added items will appear here.</p>
       </div>
-    ))}
-    <div className="total">
-      <p>Order Total ${total}</p>
-    </div>
-    <p>
-      <img src={carbonNeutralIcon} alt="Carbon neutral icon" />
-      This is a <span>carbon-neutral</span> delivery
-    </p>
-    <button onClick={confirmOrder} disabled={cartItems.length === 0}>
-      Confirm Order
-    </button>
+    ) : (
+      <>
+        {cartItems.map(({ product, quantity }) => (
+          <div key={product.name} className="cart-item">
+            <div className="cart-item-details">
+              <p>{product.name}</p>
+              <p>
+                {quantity}x @ ${product.price.toFixed(2)} $
+                {(product.price * quantity).toFixed(2)}
+              </p>
+            </div>
+            <button
+              onClick={() => removeFromCart(product.name)}
+              className="remove-button"
+            >
+              <img src={removeIcon} alt="Remove Icon" width={24} height={24} />
+            </button>
+          </div>
+        ))}
+        <div className="total">
+          <p>Order Total ${total}</p>
+        </div>
+        <p>
+          <img src={carbonNeutralIcon} alt="Carbon neutral icon" />
+          This is a <span>carbon-neutral</span> delivery
+        </p>
+        <button onClick={confirmOrder} disabled={cartItems.length === 0}>
+          Confirm Order
+        </button>
+      </>
+    )}
   </div>
 );
 
