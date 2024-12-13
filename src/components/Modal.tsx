e57e068614e5ebbc6f2471cbfac709c5883331e4
FriedAxons <friedaxons@proton.me>
2024-12-12 21:45:51 -0600

    Style Modal component

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -10,11 +10,11 @@ interface ModalProps {
 const Modal = ({ cartItems, total, onNewOrder }: ModalProps) => {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-      <div className="bg-rose-50 rounded-lg pt-9 pr-14 pb-10 pl-9 shadow-lg w-3/4 max-w-xl">
+      <div className="bg-rose-50 rounded-xl pt-9 pr-14 pb-9 pl-10 shadow-lg w-3/4 max-w-xl">
         <img
           src={confirmedIcon}
           alt="Confirmed Icon"
-          className="w-11 h-11 mb-5"
+          className="w-11 h-11 mb-6"
         />
         <h2 className="text-4xl text-rose-900 font-bold mb-2">
           Order Confirmed
@@ -22,7 +22,7 @@ const Modal = ({ cartItems, total, onNewOrder }: ModalProps) => {
         <p className="text-sm text-rose-500 mb-10">
           We hope you enjoy your food!
         </p>
-        <div className="pl-5">
+        <div className="pl-4 pb-6">
           {cartItems.map(({ product, quantity }) => (
             <div key={product.name} className="flex items-center mb-4">
               <img
@@ -47,10 +47,16 @@ const Modal = ({ cartItems, total, onNewOrder }: ModalProps) => {
             </div>
           ))}
         </div>
-        <div className="total">
-          <p>Order Total ${total}</p>
+        <div className="flex justify-between items-center pb-12">
+          <p className="text-sm text-rose-500 font-medium">Order Total</p>
+          <p className="text-2xl text-rose-900 font-extrabold">${total}</p>
         </div>
-        <button onClick={onNewOrder}>Start New Order</button>
+        <button
+          onClick={onNewOrder}
+          className="bg-red w-full text-rose-50 p-3 rounded-full"
+        >
+          Start New Order
+        </button>
       </div>
     </div>
   );
