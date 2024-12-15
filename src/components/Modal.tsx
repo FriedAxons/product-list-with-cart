645dbd5ec6a3efdfa4d61092ee4851b943eee677
FriedAxons <friedaxons@proton.me>
2024-12-15 12:26:25 -0600

    Revert "Refactor Modal styles on desktop version"
    This reverts commit e39893e9ddb4f6261f52e9e88e32d785374b2f42.

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -10,7 +10,7 @@ interface ModalProps {
 const Modal = ({ cartItems, total, onNewOrder }: ModalProps) => {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-      <div className="bg-rose-50 rounded-xl pt-9 lg:pr-9 pr-12 pb-9 lg:pl-9 pl-6 shadow-lg lg:w-3/4 lg:max-w-xl lg:h-auto h-[calc(100vh-3rem)] lg:mt-0 mt-28">
+      <div className="bg-rose-50 rounded-xl pt-9 lg:pr-14 pr-12 pb-9 lg:pl-10 pl-6 shadow-lg lg:w-3/4 lg:max-w-xl lg:h-auto h-[calc(100vh-3rem)] lg:mt-0 mt-28">
         <img
           src={confirmedIcon}
           alt="Confirmed Icon"
@@ -19,10 +19,10 @@ const Modal = ({ cartItems, total, onNewOrder }: ModalProps) => {
         <h2 className="lg:text-4xl text-[40px] text-rose-900 lg:font-bold font-extrabold mb-2">
           Order Confirmed
         </h2>
-        <p className="lg:text-sm text-rose-500 lg:mb-8 mb-12">
+        <p className="lg:text-sm text-rose-500 lg:mb-10 mb-12">
           We hope you enjoy your food!
         </p>
-        <div className="bg-rose-100 rounded-t-lg pl-4 pb-7 lg:pt-5 lg:pl-5 lg:pr-6">
+        <div className="pl-4 pb-7">
           {cartItems.map(({ product, quantity }) => (
             <div key={product.name} className="flex items-center lg:mb-4 mb-9">
               <img
@@ -47,17 +47,15 @@ const Modal = ({ cartItems, total, onNewOrder }: ModalProps) => {
             </div>
           ))}
         </div>
-        <div className="bg-rose-100 rounded-b-lg flex justify-between items-center">
-          <p className="text-sm text-rose-500 lg:pl-5 pl-[16px] font-medium">
+        <div className="flex justify-between items-center pb-12">
+          <p className="text-sm text-rose-500 pl-[16px] font-medium">
             Order Total
           </p>
-          <p className="text-2xl text-rose-900 font-extrabold lg:pr-6">
-            ${total}
-          </p>
+          <p className="text-2xl text-rose-900 font-extrabold">${total}</p>
         </div>
         <button
           onClick={onNewOrder}
-          className="lg:right-2 right-5 bg-red lg:w-full lg:w-[105%] w-[90%] text-rose-50 lg:p-3 pt-4 pb-4 mt-7 rounded-full"
+          className="lg:right-2 right-5 bg-red lg:w-full lg:w-[105%] w-[90%] text-rose-50 lg:p-3 pt-4 pb-4 rounded-full"
         >
           Start New Order
         </button>
